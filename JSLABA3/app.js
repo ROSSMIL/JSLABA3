b616d705cc926b3bb186b3a0af5f4b994bd4c178
ROSSMIL <112856396+ROSSMIL@users.noreply.github.com>
2022-10-12 15:37:51 +0300

    Add files via upload

---
+++ JSLABA3/app.js
@@ -0,0 +1,80 @@
+//Задача 1
+function chess (){
+    let uperLine = "#   #   #   # ";
+    let underLine= " #   #   #   #";
+    for(let i = 0; i <4 ;i++){
+    console.log(uperLine);
+    console.log(underLine);
+}
+}
+chess()
+
+//Задача 2
+for (var i = 1; i < 101; i++) {
+    if (i % 3 == 0) {
+        console.log("СЛАВА УКРАЇНІ!");
+    } else {
+        console.log(i);
+    }
+}
+//Задача 3
+
+let a = "" + 1 + 0;
+ console.log(a);
+
+ let b = "" - 1 + 0;
+ console.log(b);
+
+ let c = true + false;
+ console.log(c);
+
+ let d = 6 / "3";
+ console.log(d);
+
+ let e = "2"*"3";
+ console.log(e);
+
+ let f = 4 + 5 + "px";
+ console.log(f);
+
+ let g = "$ + 4 + 5";
+ console.log(g);
+
+ let h = "4" - 2;
+ console.log(h);
+
+ let j = "4px" - 2;
+ console.log(j);
+
+ let k = 7 / 0;
+ console.log(k);
+
+ let l = "-9\n" + 5;
+ console.log(l);
+
+ let m = "-9\n" - 5;
+ console.log(m);
+
+ let n = 5 && 2;
+ console.log(n);
+
+ let o = 2 && 5;
+ console.log(o);
+
+ let p = 5||0;
+ console.log(p);
+
+ let q = 0||5;
+ console.log(q);
+
+ let r = null + 1 ;
+ console.log(r);
+
+ let s = undefined + 1;
+ console.log(s);
+
+ let t = null == "\n0\n";
+ console.log(t);
+
+ let u = null == +"\n0\n";
+ console.log(u);
